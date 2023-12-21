26d5a9bdd052118f6b971ee834c6472ec620a929
georgewrmarshall <george.marshall@consensys.net>
2023-12-20 18:26:20 -0800

    Updating to working script

--- component-tracking-script.js
+++ component-tracking-script.js
@@ -1,81 +1,96 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const glob = require("glob");
+const path = require("path");
 
 const rootFolder = "ui";
-const ignoreFolder = "ui/components/component-library";
-const componentLibraryImportRegex =
-  /import {([\s\S]*?)} from ['"](\.{2,}\/component-library)['"];/g;
+const ignoreFolder = path.join(rootFolder, "components", "component-library");
 
-const components = [
+const components = new Set([
   "AvatarAccount",
-  "AvatarBase",
   "AvatarFavicon",
   "AvatarIcon",
   "AvatarNetwork",
   "AvatarToken",
+  "AvatarBase",
   "BadgeWrapper",
-  "BannerAlert",
-  "BannerBase",
-  "BannerTip",
+  "Box",
   "Button",
   "ButtonBase",
   "ButtonIcon",
   "ButtonLink",
   "ButtonPrimary",
   "ButtonSecondary",
+  "Checkbox",
+  "Container",
   "FormTextField",
   "HeaderBase",
   "HelpText",
   "Icon",
-  "Input",
   "Label",
-  "Modal",
-  "ModalContent",
-  "ModalHeader",
-  "ModalOverlay",
   "PickerNetwork",
   "Tag",
   "TagUrl",
   "Text",
+  "Input",
   "TextField",
   "TextFieldSearch",
-];
+  "ModalContent",
+  "ModalOverlay",
+  "ModalFocus",
+  "Modal",
+  "ModalBody",
+  "BannerBase",
+  "BannerAlert",
+  "BannerTip",
+  "PopoverHeader",
+  "Popover",
+  "ModalHeader",
+  "SelectButton",
+  "SelectOption",
+  "SelectWrapper",
+]);
 
-const componentInstances = {};
+const componentInstances = new Map();
 
-const processFile = (filePath) => {
-  const content = fs.readFileSync(filePath, "utf8");
-  const matches = content.match(/<(\w+)/g);
-
-  if (matches) {
-    matches.forEach((match) => {
-      const componentName = match.substring(1);
-      if (components.includes(componentName)) {
-        componentInstances[componentName] =
-          (componentInstances[componentName] || 0) + 1;
-      }
-    });
+const processFile = async (filePath) => {
+  try {
+    const content = await fs.readFile(filePath, "utf8");
+    const matches = content.match(/<([A-Z]\w*)(?=\s|\/|>)/g);
+    console.log(`Processing file ${filePath}`);
+    console.log(`Matches: ${matches}`);
+    if (matches) {
+      matches.forEach((match) => {
+        const componentName = match.substring(1);
+        if (components.has(componentName)) {
+          const count = componentInstances.get(componentName) || 0;
+          componentInstances.set(componentName, count + 1);
+          console.log(`Matched component: ${componentName}`);
+          console.log(`Current count: ${count + 1}`);
+        } else {
+          console.log(`Component not in list: ${componentName}`);
+        }
+      });
+    }
+  } catch (err) {
+    console.error(`Error reading file ${filePath}:`, err);
   }
 };
 
-glob(`${rootFolder}/**/*.js`, { ignore: [ignoreFolder] }, (err, files) => {
-  if (err) {
-    console.error("Error reading files:", err);
-    return;
-  }
-
-  files.forEach((file) => {
-    const content = fs.readFileSync(file, "utf8");
-    const importMatches = [...content.matchAll(componentLibraryImportRegex)];
-
-    if (importMatches.length > 0) {
-      processFile(file);
+glob(
+  `${rootFolder}/**/*.{js,tsx}`,
+  { ignore: [`${ignoreFolder}/**`, `${rootFolder}/**/*.test.{js,tsx}`] },
+  async (err, files) => {
+    if (err) {
+      console.error("Error reading files:", err);
+      return;
     }
-  });
 
-  console.log("Component Adoption Metrics:");
-  components.forEach((componentName) => {
-    const instanceCount = componentInstances[componentName] || 0;
-    console.log(`${componentName}: ${instanceCount} instances`);
-  });
-});
+    await Promise.all(files.map(processFile));
+
+    console.log("Component Adoption Metrics:");
+    components.forEach((componentName) => {
+      const instanceCount = componentInstances.get(componentName) || 0;
+      console.log(`${componentName}: ${instanceCount} instances`);
+    });
+  }
+);
